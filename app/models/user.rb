1b3c47d4f08866211153e6c1664da5e28bb6c61a
hiro139fiskst <takehiro.abe@don-tokyo.co.jp>
2020-10-04 21:26:09 +0900

    テストコード　修正2　不要なコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,13 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
 
-  # PASSWORD_REGEX = /\A(?=.?[a-z])(?=.?\d)[a-z\d]{8,100}+\z/i
-  # validates_format_of :password, with: PASSWORD_REGEX
-  # validates :password, length: { minimum: 6 }
+ 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])\w{6,12}\z/.freeze
   validates :password, presence: true,
                        format: { with: VALID_PASSWORD_REGEX,
                                  message: 'は半角6~12文字小文字・数字それぞれ１文字以上含む必要があります' }
 
-  # VALID_EMAIL_REGEX = /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/
-  # validates_format_of :email, with: VALID_EMAIL_REGEX
+
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
