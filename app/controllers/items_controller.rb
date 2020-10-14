310203a53e12b4f9d45dc322fd23b3edf2cfbff3
hiro139fiskst <takehiro.abe@don-tokyo.co.jp>
2020-10-14 15:18:56 +0900

    商品削除機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  # skip_before_action :authenticate_user!  ,only: [:show, :index]
   before_action :item_set, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
@@ -34,7 +33,7 @@ class ItemsController < ApplicationController
     if @item.destroy
       redirect_to root_path
     else
-      render :edit
+      render :show
     end
   end
 
