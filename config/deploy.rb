5d3fcb2b4c2cfa38a50af273f253e87c0760ba42
hiro139fiskst <takehiro.abe@don-tokyo.co.jp>
2020-10-22 22:44:38 +0900

    自動デプロイ　修正２

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,21 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock  3.14.1
+lock  '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, furima-28117
+set :application, 'furima-28117'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  git@github.com:hiro139fiskst/furima-28117.git
+set :repo_url,  'git@github.com:hiro139fiskst/furima-28117.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.6.5 #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                  keys: [~/.ssh/furima28117.pem] 
+                                  keys: ['~/.ssh/furima28117.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
